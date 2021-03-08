9f974a7305d8d6711cacc1865c6fe00d1ec9df09
RobinDenAdel <robin.den.adel@cgi.com>
2021-03-08 09:31:57 +0100

    Adapts properties and preview image.

--- src/asset-list-widget/properties.ts
+++ src/asset-list-widget/properties.ts
@@ -1,6 +1,8 @@
 export const PROPERTIES: {} = [
     {id: 1, name:"id"},
     {id: 2, name:"name"},
-    {id: 3, name: "description"},
-    {id: 4, name: "notes"}
+    {id: 3, name: "orderNr"},
+    {id: 4, name: "caseId"}, 
+    {id: 5, name: "chargeNr"}, 
+    {id: 6, name: "articleNr"}
 ]
